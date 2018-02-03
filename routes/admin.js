4398dbaa4904af4399a866098dcccbd97fe5491e
neiltcox <neiltcox@icloud.com>
2018-02-03 12:51:44 -0800

    added admin route

---
+++ routes/admin.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('admin_dash');
+});
+
+router.get('/admin/:location/:zip', function(req, res, next) {
+  var disaster = {
+    zip: req.params.zip,
+    location: req.params.location
+  };
+  res.send(zip);
+});
+
+module.exports = router;
