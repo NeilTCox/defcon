7a3d8ae610fd8ec2881e7e683a81f8f3f1d24602
neiltcox <neiltcox@icloud.com>
2018-02-03 10:36:09 -0800

    notification request and confirmation working

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,8 +1,3 @@
-Notification.requestPermission().then(function(result) {
-  alert('IM HEREEEEE');
-  console.log(result);
-});
-
 notifyMe();
 
 function notifyMe() {
@@ -10,23 +5,20 @@ function notifyMe() {
   if (!("Notification" in window)) {
     alert("This browser does not support system notifications");
   }
-
   // Let's check whether notification permissions have already been granted
   else if (Notification.permission === "granted") {
     // If it's okay let's create a notification
-    var notification = new Notification("Hi there!");
+    var notification = new Notification("You're all set up to receive alerts!");
   }
-
   // Otherwise, we need to ask the user for permission
   else if (Notification.permission !== 'denied') {
     Notification.requestPermission(function (permission) {
       // If the user accepts, let's create a notification
       if (permission === "granted") {
-        var notification = new Notification("Hi there!");
+        var notification = new Notification("You're good to go!");
       }
     });
   }
-
   // Finally, if the user has denied notifications and you
   // want to be respectful there is no need to bother them any more.
 }
