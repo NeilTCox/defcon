ff3cfc89609febc79486786c054e67476087bdb0
neiltcox <neiltcox@icloud.com>
2018-02-04 11:01:23 -0800

    morning commit

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,6 +1,5 @@
 $(function() {
   $('.list-inline').on('click', '#shelterButton', function(event){
-    alert('one');
     $("#locationId").addClass("hidden");
     $("#hospitalId").addClass("hidden");
     $("#foodId").addClass("hidden");
@@ -9,7 +8,6 @@ $(function() {
   });
 
   $('.list-inline').on('click', '#foodButton', function(event){
-    alert('two');
     $("#locationId").addClass("hidden");
     $("#hospitalId").addClass("hidden");
     $("#shelterId").addClass("hidden");
@@ -18,7 +16,6 @@ $(function() {
   });
 
   $('.list-inline').on('click', '#hospitalButton', function(event){
-    alert('three');
     $("#locationId").addClass("hidden");
     $("#shelterId").addClass("hidden");
     $("#foodId").addClass("hidden");
